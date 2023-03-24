48b3afaf46ba70e26d51b8df944959bd60464a4e
Mateusz Kocot <mateuszkocot99@gmail.com>
2023-03-24 16:07:55 +0100

    Refactor + add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# pps-timing-ml-sampic
+
+Before using, download the data folder from Cernbox (contains input, final dataset and NN weights).
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from pathlib import Path
 
+
 def save_plt(plot: plt, path: Path | str, **kwargs) -> None:
     if isinstance(path, str):
         path = Path(path)

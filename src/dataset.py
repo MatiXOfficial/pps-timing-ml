b7b5c0336900c5205be880ffb6af0d4fcf175cec
Mateusz Kocot <mateuszkocot99@gmail.com>
2023-05-20 17:09:42 +0200

    Added training on other channels

--- src/dataset.py
+++ src/dataset.py
@@ -34,3 +34,52 @@ def load_dataset_train_val(
     x_train, x_val, y_train, y_val = train_test_split(x_base_train, y_base_train, test_size=test_size,
                                                       random_state=random_state)
     return x_train, x_val, y_train, y_val
+
+
+def load_dataset_all_channels(pwd: Path) -> tuple[dict[tuple, np.ndarray], dict[tuple, np.ndarray]]:
+    with open(pwd / DATASET_ROOT_PATH, 'rb') as file:
+        dataset = pickle.load(file)
+
+    all_X, all_y = {}, {}
+    for (plane, channel), (x, y) in dataset.items():
+        all_X[(plane, channel)] = x
+        all_y[(plane, channel)] = y
+    return all_X, all_y
+
+
+def load_dataset_train_test_all_channels(
+        pwd: Path, test_size: float = 0.2, random_state: int = 42
+) -> tuple[dict[tuple, np.ndarray], dict[tuple, np.ndarray], dict[tuple, np.ndarray], dict[tuple, np.ndarray]]:
+    all_X, all_y = load_dataset_all_channels(pwd)
+
+    x_train, x_test, y_train, y_test = {}, {}, {}, {}
+    for (plane, channel), x in all_X.items():
+        y = all_y[(plane, channel)]
+
+        x_train_ch, x_test_ch, y_train_ch, y_test_ch = train_test_split(x, y, test_size=test_size,
+                                                                        random_state=random_state)
+        x_train[(plane, channel)] = x_train_ch
+        x_test[(plane, channel)] = x_test_ch
+        y_train[(plane, channel)] = y_train_ch
+        y_test[(plane, channel)] = y_test_ch
+
+    return x_train, x_test, y_train, y_test
+
+
+def load_dataset_train_val_all_channels(
+        pwd: Path, test_size: float = 0.2, random_state: int = 42
+) -> tuple[dict[tuple, np.ndarray], dict[tuple, np.ndarray], dict[tuple, np.ndarray], dict[tuple, np.ndarray]]:
+    all_X, _, all_y, _ = load_dataset_train_test_all_channels(pwd)
+
+    x_train, x_val, y_train, y_val = {}, {}, {}, {}
+    for (plane, channel), x in all_X.items():
+        y = all_y[(plane, channel)]
+
+        x_train_ch, x_val_ch, y_train_ch, y_val_ch = train_test_split(x, y, test_size=test_size,
+                                                                      random_state=random_state)
+        x_train[(plane, channel)] = x_train_ch
+        x_val[(plane, channel)] = x_val_ch
+        y_train[(plane, channel)] = y_train_ch
+        y_val[(plane, channel)] = y_val_ch
+
+    return x_train, x_val, y_train, y_val

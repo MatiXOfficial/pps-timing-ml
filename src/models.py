4c5fff0acdfea7781eede1f40ca19e4f6fcca12e
Mateusz Kocot <mateuszkocot99@gmail.com>
2023-06-16 22:55:25 +0200

    Trained ITI on using smaller model

--- src/models.py
+++ src/models.py
@@ -237,3 +237,7 @@ optimal_model_builders_all_ch = OptimalModelBuilders(
 )
 
 optimal_model_builder_all_ch = optimal_model_builders_all_ch.unet
+
+optimal_model_builder_iti = lambda: unet_builder(hp_unet_depth=2, hp_n_conv_layers=2, hp_filters_mult=4,
+                                                 hp_spatial_dropout=0.1, hp_batch_normalization=True,
+                                                 hp_input_batch_normalization=True, hp_normalize_signal=False)
